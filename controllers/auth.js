dfe73e6c1579004884d7b1e6b4d29964d72da470
Syed Qadri <asjad_qadri@hotmail.com>
2023-09-29 13:58:05 -0500

    Login working for tutor and student

--- controllers/auth.js
+++ controllers/auth.js
@@ -16,6 +16,7 @@ exports.register = (req, res) => {
     const email = req.body.email
     const password = req.body.password
     const passwordconfirm = req.body.passwordconfirm
+    const type = req.body.type
 
     db.query('SELECT Username from Users where Username = ?', [email], async (err, results) =>{
         console.log(results)
@@ -34,19 +35,85 @@ exports.register = (req, res) => {
         }
 
         let hashedPassword = await bcrypt.hash(password, 8);
-        console.log(hashedPassword);
+        console.log(hashedPassword)
 
-        db.query('INSERT INTO Users SET ? ', {Name: name, Username: email, Password: hashedPassword}, (err, results) =>{
-            if(err){
-                console.log("ERROR: "+ err);
-            }
-            else{
-                res.render('register', {
-                    message: 'User successfully registered'
-                })
-            }
-        })
+        if(type == 'student'){
+            db.query('INSERT INTO Users SET ? ', {Name: name, Username: email, Password: hashedPassword}, (err, results) =>{
+                if(err){
+                    console.log("ERROR: "+ err);
+                }
+                else{
+                    res.render('login', {
+                        message: 'User successfully registered'
+                    })
+                }
+            })
+        }
+        else{
+            db.query('INSERT INTO Tutor SET ? ', {Name: name, Username: email, Password: hashedPassword}, (err, results) =>{
+                if(err){
+                    console.log("ERROR: "+ err);
+                }
+                else{
+                    res.render('login', {
+                        message: 'User successfully registered'
+                    })
+                }
+            })
+        }
+        
     }) 
 
 }
 
+exports.login = (req, res) => {
+    console.log(req.body);
+    const email = req.body.email
+    const password = req.body.password
+
+    db.query('SELECT Username, Password from Users where Username = ?', [email], async (err, results) =>{
+        console.log(results)
+        if(err){
+            console.log("ERROR: " + err)
+        }
+        if(results.length == 0){
+            db.query('SELECT Username, Password from Tutor where Username = ?', [email], async (err, results) =>{
+                console.log(results[0])
+                if(err){
+                    console.log("ERROR: " + err)
+                }
+                if(results.length == 0){
+                    return res.render('login', {
+                        message: 'Email has not been registered'
+                    })
+                }
+                let match = await bcrypt.compare(password, results[0].Password)
+                console.log(match)
+                if(match){
+                    return res.render('index');
+                } else {
+                    return res.render('login', {
+                        message: 'Invalid password'
+                    });
+                }
+            })
+            // return res.render('login', {
+            //     message: 'Email has not been registered'
+            // })
+        }
+        else{
+            let match = await bcrypt.compare(password, results[0].Password)
+            console.log(match)
+            if(match){
+                return res.render('index');
+            } else {
+                return res.render('login', {
+                    message: 'Invalid password'
+                });
+            }
+        }
+
+
+    })
+    
+}
